60f34a71767400854508f9091c5a9d8935adec02
lucascvidal <lucascezerevidal@outlook.com>
2019-03-27 11:53:23 -0300

    Currency cards styled

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,15 +4,29 @@ const Carousel = (props) => {
 
     return (
 
-        props.currencies.map((item) =>
+        <div className="cards">
+            {props.currencies.map((item) =>
 
-            <div key={item.id}>
-                <h1>{item.name}</h1>
-                <button>{item.symbol}</button>
-                <p>{item.code}</p>
-                <h1>{item.amount}</h1>
-            </div>
-        )
+                <div className={
+
+
+                    (item.id % 5 === 1) ? "card-container background-primary" :
+                        (item.id % 5 === 2) ? "card-container background-secondary" :
+                            (item.id % 5 === 3) ? "card-container background-tertiary" :
+                                (item.id % 5 === 4) ? "card-container background-quaternary" : "card-container background-tertiary"
+
+                } key={item.id}>
+                    <div className="card-description">
+                        <h1>{item.name}</h1>
+                        <p className="card-symbol">{item.symbol}</p>
+                        <p>{item.code}</p>
+                    </div>
+                    <div className="card-amount">
+                        <h1>{item.amount}</h1>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
