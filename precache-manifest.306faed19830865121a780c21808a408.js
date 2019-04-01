998a337ae4014c1069ff566690c0f0ee8f726d2c
lucascvidal <lucascezerevidal@outlook.com>
2019-04-01 14:13:38 -0300

    Updates

---
+++ precache-manifest.306faed19830865121a780c21808a408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5c11322258a35487320aa888ee4d8e0",
+    "url": "/my-wallet/static/media/bg-image.a5c11322.svg"
+  },
+  {
+    "revision": "1ed893a5c4f2134e3b6f3bb6e2ae315a",
+    "url": "/my-wallet/static/media/bg-card.1ed893a5.svg"
+  },
+  {
+    "revision": "cd3ccae70da1f18d1610",
+    "url": "/my-wallet/static/js/runtime~main.03d6ddd9.js"
+  },
+  {
+    "revision": "26aea75f037b850ee7b5",
+    "url": "/my-wallet/static/js/main.056c3e1d.chunk.js"
+  },
+  {
+    "revision": "ad67d665b114896bcf1d",
+    "url": "/my-wallet/static/js/2.58b3c62a.chunk.js"
+  },
+  {
+    "revision": "26aea75f037b850ee7b5",
+    "url": "/my-wallet/static/css/main.d804fb67.chunk.css"
+  },
+  {
+    "revision": "e1256db672ca5d14a811983b37fbeece",
+    "url": "/my-wallet/index.html"
+  }
+];
